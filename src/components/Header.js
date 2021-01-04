afae09d0907d5585b1bcb0f011e221535799c5c9
Takeshi Shimada <takeshi.shimada@poli.ufrj.br>
2021-01-04 16:19:32 -0300

    Website update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,13 @@ import React from 'react'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-diamond"></span>
+      <span className="icon fa-anchor"></span>
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
-        <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+        <h1>UFRJ Nautilus</h1>
+        <p>A UFRJ Nautilus é a única equipe sul-americana <br /> a participar da <a href="https://robosub.org/programs/2021/">RoboSub</a> - maior competição  anual de Veículos<br /> Submarinos Autônomos do mundo.
+          
         </p>
       </div>
     </div>
